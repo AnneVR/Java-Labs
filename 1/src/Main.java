52453ae4c098cad95d0b2e075af9b501bb7cace5
Anna Dudina <44443745+AnneVR@users.noreply.github.com>
2018-11-01 16:10:52 +0300

    updated Main.java

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
-public class Main {
 
+public class Main 
+    {
+        public static void main(String[] args) 
+            {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Matrix size: ");
+                int n = in.nextInt();
+                int[][] array = new int[n][n];
+                for (int i = 0; i < n; i++) 
+                {
+                    for (int j = 0; j < n; j++) 
+                    {
+                        if ((i == j)||(i+j==n-1)) 
+                        {
+                            array[i][j] = 1;
+                        } else {
+                            array[i][j] = 0;
+                        }
+                    }
+                 }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Matrix size: ");
-        int n = in.nextInt();
-        int[][] array = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == j)||(i+j==n-1)) {
-                    array[i][j] = 1;
-                } else {
-                    array[i][j] = 0;
+                for (int a = 0; a < n; a++) 
+                {
+                    for (int b = 0; b < n; b++) 
+                    {
+                        System.out.print(array[a][b] + "\t");
+                    }
+                    System.out.println();
                 }
-            }
-        }
-
-        for (int a = 0; a < n; a++) {
-                for (int b = 0; b < n; b++) {
-                    System.out.print(array[a][b] + "\t");
-                }
-                System.out.println();
-        }
+             }
     }
-}
